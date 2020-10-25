900b63efda8a86afd31aa25f51ac405f7726f089
thiagoanjos <thiago.w3c@gmail.com>
2020-10-25 16:45:21 -0300

    criando usuários

---
+++ src/controllers/UserController.js
@@ -0,0 +1,13 @@
+import User from '../models/User';
+
+class UseController {
+  async store(req, res) {
+    try {
+      const newUser = await User.create(req.body);
+      res.json(newUser);
+    } catch (e) {
+      res.status(400).json({ errors: e.errors.map((error) => error.message) });
+    }
+  }
+}
+export default new UseController();
---
+++ src/models/User.js
@@ -0,0 +1,49 @@
+import Sequelize, { Model } from 'sequelize';
+import bcryptjs from 'bcryptjs';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validade: {
+          len: {
+            args: [3, 255],
+            msg: 'Campo nome deve ter entre 3 e 255 caracteres',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'E-mail já existe',
+        },
+        validade: {
+          isEmail: {
+            msg: 'E-mail inválido',
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validade: {
+          len: {
+            args: [6, 50],
+            msg: 'Campo senha deve ter entre 6 e 50 caracteres',
+          },
+        },
+      },
+    }, { sequelize });
+    this.addHook('beforeSave', async (user) => {
+      user.password_hash = await bcryptjs.hash(user.password, 8);
+    });
+    return this;
+  }
+}
