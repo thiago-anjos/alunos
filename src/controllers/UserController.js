48f0398d451fef7cd559b69db5bb1c591bf6af8d
thiagoanjos <thiago.w3c@gmail.com>
2020-10-26 20:36:13 -0300

    usuários não podem ver outros usuários

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,7 +4,8 @@ class UseController {
   async store(req, res) {
     try {
       const newUser = await User.create(req.body);
-      return res.json(newUser);
+      const { id, nome, email } = newUser;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.status(400).json({ errors: e.errors.map((error) => error.message) });
     }
@@ -13,7 +14,7 @@ class UseController {
   // index
   async index(req, res) {
     try {
-      const users = await User.findAll();
+      const users = await User.findAll({ attributes: ['id', 'nome', 'email'] });
       console.log('USER ID', req.userId);
       console.log('USER EMAIl', req.userEmail);
       return res.json(users);
@@ -26,7 +27,8 @@ class UseController {
   async show(req, res) {
     try {
       const user = await User.findByPk(req.params.id);
-      return res.json(user);
+      const { id, nome, email } = user;
+      return res.json({ id, nome, email });
     } catch (error) {
       return res.json(null);
     }
@@ -35,11 +37,11 @@ class UseController {
   // update
   async update(req, res) {
     try {
-      if (!req.params.id) return res.status(400).json({ errors: ['ID não enviado'] });
-      const user = await User.findByPk(req.params.id);
+      const user = await User.findByPk(req.userId);
       if (!user) return res.status(400).json({ errors: ['Usuário não encontrado'] });
       const newDataUser = await user.update(req.body);
-      return res.json(newDataUser);
+      const { id, nome, email } = newDataUser;
+      return res.json({ id, nome, email });
     } catch (error) {
       return res.json(null);
     }
@@ -48,11 +50,10 @@ class UseController {
   // delete
   async delete(req, res) {
     try {
-      if (!req.params.id) return res.status(400).json({ errors: ['ID não enviado'] });
-      const user = await User.findByPk(req.params.id);
+      const user = await User.findByPk(req.userId);
       if (!user) return res.status(400).json({ errors: ['Usuário não encontrado'] });
       await user.destroy();
-      return res.json(user);
+      return res.json(null);
     } catch (error) {
       return res.json(null);
     }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,9 +3,10 @@ import userController from '../controllers/UserController';
 import loginRequired from '../middlewares/LoginRequired';
 
 const router = new Router();
+// router.get('/', userController.index);
+// router.get('/:id', userController.show);
+
 router.post('/', userController.store);
-router.get('/', loginRequired, userController.index);
-router.get('/:id', userController.show);
-router.put('/:id', userController.update);
-router.delete('/:id', userController.delete);
+router.put('/', loginRequired, userController.update);
+router.delete('/', loginRequired, userController.delete);
 export default router;
