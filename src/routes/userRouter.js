87e5fec832c601b0799940d5e7a05e0965d8bbb3
thiagoanjos <thiago.w3c@gmail.com>
2020-10-27 10:46:39 -0300

    seeds

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,8 +3,8 @@ import userController from '../controllers/UserController';
 import loginRequired from '../middlewares/LoginRequired';
 
 const router = new Router();
-// router.get('/', userController.index);
-// router.get('/:id', userController.show);
+router.get('/', userController.index);
+router.get('/:id', userController.show);
 
 router.post('/', userController.store);
 router.put('/', loginRequired, userController.update);
