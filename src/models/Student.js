b39c087d761ca622f47fab891b7819ebd9ad93f4
thiagoanjos <thiago.w3c@gmail.com>
2020-11-01 07:22:27 -0300

    crud-students complete

--- src/models/Student.js
+++ src/models/Student.js
@@ -3,12 +3,65 @@ import Sequelize, { Model } from 'sequelize';
 export default class Student extends Model {
   static init(sequelize) {
     super.init({
-      nome: Sequelize.STRING,
-      sobrenome: Sequelize.STRING,
-      email: Sequelize.STRING,
-      idade: Sequelize.INTEGER,
-      peso: Sequelize.FLOAT,
-      altura: Sequelize.FLOAT,
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validade: {
+          len: {
+            args: [3, 255],
+            msg: 'Campo nome deve ter entre 3 e 255 caracteres',
+          },
+        },
+      },
+      sobrenome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validade: {
+          len: {
+            args: [3, 255],
+            msg: 'Campo nome deve ter entre 3 e 255 caracteres',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'E-mail já existe',
+        },
+        validade: {
+          isEmail: {
+            msg: 'E-mail inválido',
+          },
+        },
+      },
+      idade: {
+        type: Sequelize.INTEGER,
+        defaultValue: '',
+        validade: {
+          isInt: {
+            msg: 'Idade precisa ser um número inteiro',
+          },
+        },
+      },
+      peso: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validade: {
+          isFloat: {
+            msg: 'Peso precisa ser um número',
+          },
+        },
+      },
+      altura: {
+        type: Sequelize.INTEGER,
+        defaultValue: '',
+        validade: {
+          isInt: {
+            msg: 'Altura precisa ser um número',
+          },
+        },
+      },
     }, { sequelize });
     return this;
   }
--- src/routes/StudentRoutes.js
+++ src/routes/StudentRoutes.js
@@ -1,6 +1,14 @@
 import { Router } from 'express';
 import studentController from '../controllers/StudentController';
+import loginRequired from '../middlewares/LoginRequired';
 
 const router = new Router();
+
 router.get('/', studentController.index);
+router.get('/:id', studentController.show);
+
+router.post('/', loginRequired, studentController.store);
+router.put('/:id', loginRequired, studentController.update);
+router.delete('/:id', loginRequired, studentController.delete);
+
 export default router;
