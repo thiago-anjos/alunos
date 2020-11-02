0a6390ba6a5e1207d25bdd3ad5e17bc4d37cfe20
thiagoanjos <thiago.w3c@gmail.com>
2020-11-02 18:22:56 -0300

    static files

--- src/models/Photo.js
+++ src/models/Photo.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import appConfig from '../config/appConfig';
 
 export default class Photo extends Model {
   static init(sequelize) {
@@ -19,6 +20,12 @@ export default class Photo extends Model {
           notEmpty: {
             msg: 'Campo não pode ser vázio',
           },
+        },
+      },
+      url: {
+        type: Sequelize.VIRTUAL,
+        get() {
+          return `${appConfig.url}/images/${this.getDataValue('filename')}`;
         },
       },
     }, { sequelize, tableName: 'photos' });
