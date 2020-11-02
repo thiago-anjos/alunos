a7762eb0a962eac67b7bde375d4b50dc6f01462b
thiagoanjos <thiago.w3c@gmail.com>
2020-11-02 09:17:22 -0300

    upload photos to database

--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -1,17 +1,22 @@
 import multer from 'multer';
 import multerConfig from '../config/multerConfig';
+import Photo from '../models/Photo';
 
 const upload = multer(multerConfig).single('fileUpload');
 
 class PhotoController {
-  async store(req, res) {
-    return upload(req, res, (err) => {
+  store(req, res) {
+    return upload(req, res, async (err) => {
       if (err) {
         return res.status(400).json({
           errors: [err.code],
         });
       }
-      return res.json(req.file);
+      const { originalname, filename } = req.file;
+      const { student_id } = req.body;
+      console.log('student_id', student_id);
+      const newPhoto = await Photo.create({ originalname, filename, student_id });
+      return res.json(newPhoto);
     });
   }
 }
